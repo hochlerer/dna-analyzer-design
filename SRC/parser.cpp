03458936ddd9e2f5e65e16b5de23eaadb296b0dc
hochlerer <hb@swm-wolf.org>
2020-07-05 16:13:21 +0300

    Parser class declaration

---
+++ SRC/parser.cpp
@@ -0,0 +1,10 @@
+//
+// Created by y on 7/5/20.
+//
+
+#include "parser.h"
+
+void Parser::parseCmd(const std::string cmd) {
+
+}
+
---
+++ SRC/parser.h
@@ -0,0 +1,21 @@
+//
+// Created by y on 7/5/20.
+//
+
+#ifndef DNA_PARSER_H
+#define DNA_PARSER_H
+#include <string>
+#include <vector>
+
+class Parser{
+public:
+    void parseCmd(const std::string cmd);
+    const std::string& getCmdName()const {return m_commandName;}
+    const std::vector<std::string>& getParams()const {return m_params;}
+
+private:
+    std::string m_commandName;
+    std::vector<std::string> m_params;
+};
+
+#endif //DNA_PARSER_H
