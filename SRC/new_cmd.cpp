f07eaaedf6c33152774bafa323ab93a7c1d48072
hochlerer <hb@swm-wolf.org>
2020-07-05 15:01:21 +0300

    NawCMD class declaration

---
+++ SRC/new_cmd.cpp
@@ -0,0 +1,8 @@
+//
+// Created by y on 7/5/20.
+//
+
+
+#include "new_cmd.h"
+
+
---
+++ SRC/new_cmd.h
@@ -0,0 +1,14 @@
+//
+// Created by y on 7/5/20.
+//
+
+#ifndef DNA_NEW_CMD_H
+#define DNA_NEW_CMD_H
+
+#include "creation_command.h"
+
+class NewCMD: public CreationCMD{
+    void run();
+};
+
+#endif //DNA_NEW_CMD_H
