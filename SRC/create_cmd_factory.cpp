7ff0a249fd2b1ba5c41aa235593cd88ee1cc885d
hochlerer <hb@swm-wolf.org>
2020-07-07 19:46:45 +0300

    dup command implementation

--- SRC/create_cmd_factory.cpp
+++ SRC/create_cmd_factory.cpp
@@ -4,13 +4,20 @@
 
 #include "creation_command.h"
 #include "new_cmd.h"
-
+#include "load_cmd.h"
+#include "dup_cmd.h"
 #include "create_cmd_factory.h"
 
 
 ICMD *CreateCmdFactory::create(const Parser& p) {
     if(p.getCmdName() == "new"){
-        return new NewCMD(p);
+        return new NewCmd(p);
+    }
+    if(p.getCmdName() == "load"){
+        return new LoadCmd(p);
+    }
+    if(p.getCmdName() == "dup"){
+        return new DupCmd(p);
     }
     return NULL;
 }
